4a80fe96f9a912006361373e2af687857eaff756
Sajeeb <sajeeb2186@gmail.com>
2025-04-04 22:50:40 +0600

    n

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -4,7 +4,7 @@ const connectMongodb=async()=>{
     try {
 
        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("connected to mongodb")
+       // console.log("connected to mongodb")
         
     } catch (error) {
         console.log(error);
